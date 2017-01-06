b1830af0fc193f413b225bd2bc759e9a96c4f230
jason brown <j.grant.brown@gmail.com>
2017-01-06 14:31:24 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,18 @@
+#TEST usin each
 def reverse_each_word(sentence)
-words=sentence.split(" ")
-new_sentence=[]
-words.each do |word|
-  word.reverse
+ words=sentence.split(" ")
+new_sentence=words.each{ |x| puts x.reverse}
 end
- new_sentence.push(word)
- end
- puts new_sentence.join(" ")
 
+reverse_each_word("this is another sentence")
 
-
+#TEST USING COLLECT
+def reverse_each_word(sentence)
+ new_sentence=[]
+ words=sentence.split(" ")
+new_sentence=words.collect{ |x| x.reverse}
+return new_sentence.join(" ")
 
 end
+
+reverse_each_word("this is another sentence")
